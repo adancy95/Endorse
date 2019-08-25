bfaa8fcdf91b842e7d2ee985e440e28bacbf1eac
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-08-25 08:44:26 -0400

    added user model

---
+++ Endorse/models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  }
+},
+{
+  timestamps: {createdAt: "created_at", updatedAt: "updated_at"}
+})
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
