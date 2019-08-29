5c6ed105b32ec6477d5640ba3e3c82f6cafa4101
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-08-28 21:04:12 -0400

    added rendering views for the get all test artifacts routes

--- Endorse/routes/testArtifacts.js
+++ Endorse/routes/testArtifacts.js
@@ -5,17 +5,39 @@ const TestArtifacts = require('../models/testartifact');
 
 router.get('/testartifacts', (req, res, next) => {
   TestArtifacts.find()
-  .then(tests => res.json(tests))
+  .then(tests => {
+    res.render('TestArtifacts/allTestArtifacts', {tests})
+  })
   .catch(err => next(err))
 })
 
 router.get('/testartifacts/artifact/:id', (req, res, next) => {
   TestArtifacts.findById(req.params.id)
-  .then(test => res.json(test))
+  .then(test => {
+    res.render('TestArtifacts/ticketDetails', {test})
+  })
   .catch(err => next(err))
 })
 
 router.post('/testartifacts/create', (req, res, next) => {
+  TestArtifacts.create({
+    ticketNumber: req.body.ticketNumber,
+    issueSummary: req.body.issueSummary,
+    jiraLink: req.body.jiraLink,
+    testEnvironment: req.body.testEnvironment,
+    issueType: req.body.issueType,
+    sprint: req.body.sprint,
+    tester: req.body.tester,
+    testStatus: req.body.testStatus,
+    testCases: req.body.testCases,
+    storyDescription: req.body.storyDescription,
+    project: req.body.project
+    
+  })
+  .then(artifact => {
+    res.redirect('/testartifacts')
+  })
+  .catch(err => next(err))
   
 })
 
