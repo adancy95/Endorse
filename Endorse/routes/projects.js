074bc2d580623ef0e5f1186d6a226be4b87e49a4
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-08-28 21:03:34 -0400

    add a project details route

--- Endorse/routes/projects.js
+++ Endorse/routes/projects.js
@@ -1,28 +1,46 @@
 const express  = require("express");
 const router   = express.Router();
 const Projects = require('../models/project');
+const TestArtifacts = require('../models/testartifact');
 
 router.get('/projects', (req, res, next) => {
   Projects.find()
-  .then(projects => res.json(projects))
+  .then(projects => {
+    res.render('Projects/allProjects', {projects})
+  })
+  .catch(err => next(err))
+})
+
+router.get('/Projects/:id', (req, res, next) => {
+  TestArtifacts.find({project: req.params.id }).populate('project')
+  .then(tickets => {
+    res.render('Projects/projectDetails', {tickets})
+  })
   .catch(err => next(err))
 })
 
 router.post('/projects/create', (req, res, next) => {
   Projects.create({projectName: req.body.projectName})
-  .then(project => res.json(project))
+  .then(project => {
+    res.redirect('/projects')
+  })
   .catch(err => next(err))
 })
 
 router.post('/editproject/:id', (req, res, next) => {
   Projects.findByIdAndUpdate(req.params.id, {projectName: req.body.projectName})
-  .then(project => res.json(project))
+  .then(project => {
+      res.redirect('/projects')
+  })
   .catch(err => next(err))
 })
 
-router.delete('/projects/:id', (req, res, next) => {
+router.post('/deleteproject/:id', (req, res, next) => {
+  console.log(req.params.id)
   Projects.findByIdAndDelete(req.params.id)
-  .then(project => console.log("projected was deleted"))
+  .then(project => {
+    res.redirect('/projects')
+  })
   .catch(err => next(err))
 })
 
