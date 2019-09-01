53572351dd28297331fc151110e2af8576bc2a2b
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-09-01 08:16:11 -0400

    modified CRUD routes. All routes return json instead of a view

--- Endorse/routes/projects.js
+++ Endorse/routes/projects.js
@@ -3,43 +3,42 @@ const router   = express.Router();
 const Projects = require('../models/project');
 const TestArtifacts = require('../models/testartifact');
 
-router.get('/projects', (req, res, next) => {
+router.get('/api/projects', (req, res, next) => {
   Projects.find()
   .then(projects => {
-    res.render('Projects/allProjects', {projects})
+    res.json(projects)
   })
   .catch(err => next(err))
 })
 
-router.get('/Projects/:id', (req, res, next) => {
+router.get('/api/projects/:id', (req, res, next) => {
   TestArtifacts.find({project: req.params.id }).populate('project')
-  .then(tickets => {
-    res.render('Projects/projectDetails', {tickets})
+  .then(artifacts => {
+    res.json(artifacts)
   })
   .catch(err => next(err))
 })
 
-router.post('/projects/create', (req, res, next) => {
+router.post('/api/projects/create', (req, res, next) => {
   Projects.create({projectName: req.body.projectName})
   .then(project => {
-    res.redirect('/projects')
+    res.json(project)
   })
   .catch(err => next(err))
 })
 
-router.post('/editproject/:id', (req, res, next) => {
+router.put('/api/projects/update/:id', (req, res, next) => {
   Projects.findByIdAndUpdate(req.params.id, {projectName: req.body.projectName})
   .then(project => {
-      res.redirect('/projects')
+      res.json([{response: "The project was updated"}, project])
   })
   .catch(err => next(err))
 })
 
-router.post('/deleteproject/:id', (req, res, next) => {
-  console.log(req.params.id)
+router.delete('/api/projects/delete/:id', (req, res, next) => {
   Projects.findByIdAndDelete(req.params.id)
   .then(project => {
-    res.redirect('/projects')
+    res.json([{response: "The project was deleted"}, project])
   })
   .catch(err => next(err))
 })
