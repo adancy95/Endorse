09cee09a57de87eb5809153eea4efcce6617e687
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-09-04 16:02:38 -0400

    added get project and get users request for the left nav bar items. Also, added a post request for creating weekly statuses.

--- Endorse/public/javascripts/script.js
+++ Endorse/public/javascripts/script.js
@@ -2,7 +2,46 @@ document.addEventListener('DOMContentLoaded', () => {
 
   $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
+    
+
   })
+
+  $(document).on("click", "#save-status", function(event){
+     let weeklyStatus = $('#create-weekly-status input')
+     event.preventDefault()
+     axios.post('/api/weeklystatus/create', {
+      tester: weeklyStatus.first().val(),
+      beginDate: weeklyStatus.eq(1).val() ,
+      endDate: weeklyStatus.eq(2).val() ,
+      bugsCreated: weeklyStatus.eq(4).val() ,
+      ticketsRejected: weeklyStatus.eq(5).val() ,
+      stories: weeklyStatus.eq(3).val() ,
+      blockers: weeklyStatus.eq(6).val() ,
+      general: weeklyStatus.eq(7).val() ,
+      nextWeek: weeklyStatus.eq().val() ,
+      testCases: weeklyStatus.eq(8).val() 
+    })
+    .then(response => {})
+    .catch(err => console.log(err))
+
+  });
+
+  axios.get('/api/projects')
+  .then(projects => {
+    projects.data.forEach(project => {
+      $('#project-links').after(`<a class="nav-link" href="#">${project.projectName}</a>`)
+    });
+  })
+  .catch(err => console.log(err))
+
+  axios.get('/users')
+  .then(users => {
+    users.data.forEach(user => {
+      $('#user-links').after(`<a class="nav-link" href="#">${user.firstName} ${user.lastName}</a>`)
+    })
+  })
+  .catch(err => console.log(err))
+  
 
 
 }, false);
