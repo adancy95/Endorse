c21b958446f5db120f8f6c3d3bb24647dc9fa4b9
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-09-04 16:06:28 -0400

    created ab event listner to delete weekly statuses

--- Endorse/public/javascripts/weeklyStatus.js
+++ Endorse/public/javascripts/weeklyStatus.js
@@ -1,7 +1,13 @@
-import { runInNewContext } from "vm";
-
 document.addEventListener('DOMContentLoaded', () => {
 
+  $(document).on("click", "#delete-status", function(event){
+    let statusId = $('#status-id').val()
+    event.preventDefault()
+    axios.delete(`/api/weeklystatus/delete/${statusId}`)
+   .then(response => {console.log(response)})
+   .catch(err => console.log(err))
+
+ });
  
 
 
