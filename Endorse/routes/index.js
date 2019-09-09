93564de7810b9dc429d26fe0546fb817310dcaa3
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-09-09 13:43:03 -0400

    removed the default layout from the index page

--- Endorse/routes/index.js
+++ Endorse/routes/index.js
@@ -4,7 +4,7 @@ const router  = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-    res.render('index');
+    res.render('index', {layout: false});
 });
 
 module.exports = router;
