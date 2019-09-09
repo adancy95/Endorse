fc9646e5f51cb1e0b0f8ef57fb9d5b0fb4bb39ef
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-09-09 19:06:28 -0400

    changed db variable

--- Endorse/app.js
+++ Endorse/app.js
@@ -18,7 +18,7 @@ const methodOverride = require('method-override')
 
 
 mongoose
-  .connect('mongodb://localhost/endorse', {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
