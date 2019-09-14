ee27997084858a9375166084d1fd78da87b440f1
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-09-14 07:08:09 -0400

    added middleware to protect some routes

--- routes/weeklyStatus.js
+++ routes/weeklyStatus.js
@@ -4,6 +4,14 @@ const WeeklyStatus = require('../models/weeklyStatus');
 const TestArtifacts = require('../models/testartifact');
 const moment = require('moment')
 
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    res.redirect('/login')
+  }
+}
+
 
 router.get('/weeklystatus/:searchType', (req, res, next) => {
   let searchQuery;
@@ -16,10 +24,17 @@ router.get('/weeklystatus/:searchType', (req, res, next) => {
   }
   WeeklyStatus.find(searchQuery).populate('tester').sort({updated_at: -1})
   .then(statuses => {
+    
     statuses.map(status => {
       status.formattedBeginDate = moment(status.beginDate).format("MMMM Do, YYYY"); 
       status.formattedEndDate = moment(status.endDate).format("MMMM Do, YYYY")
       status.formattedUpdatedDate = moment(status.updated_at).format("MMMM Do YYYY, h:mm a")
+      if( req.user && req.user._id.equals(status.tester._id)){
+        status.statusOwnerTrue = "true";
+      } else{
+        status.statusOwnerFalse = "false";
+        req.flash('error', "Permission Error: Do Not Have Permission to Delete or Edit This Item ")
+      }
     });
     res.render('WeeklyStatus/allWeeklyStatus', {statuses})
   })
@@ -47,7 +62,7 @@ router.get('/api/weeklystatus/status/:id', (req, res, next) => {
   .catch(err => next(err))
 })
 
-router.post('/api/weeklystatus/create', (req, res, next) => {
+router.post('/api/weeklystatus/create', ensureAuthenticated, (req, res, next) => {
   WeeklyStatus.create({
     tester: req.body[0].value,
     beginDate: req.body[1].value,
@@ -67,7 +82,7 @@ router.post('/api/weeklystatus/create', (req, res, next) => {
   
 })
 
-router.put('/api/weeklystatus/update/:id', (req, res, next) => {
+router.put('/api/weeklystatus/update/:id', ensureAuthenticated, (req, res, next) => {
   
   WeeklyStatus.findByIdAndUpdate(req.params.id, {
     tester: req.body[0].value,
@@ -87,7 +102,7 @@ router.put('/api/weeklystatus/update/:id', (req, res, next) => {
   .catch(err => next(err))
 })
 
-router.delete('/api/weeklystatus/delete/:id', (req, res, next) => {
+router.delete('/api/weeklystatus/delete/:id', ensureAuthenticated, (req, res, next) => {
   WeeklyStatus.findByIdAndDelete(req.params.id)
   .then(deletedStatus => {
     res.json({response: "The weekly status was deleted" + deletedStatus})
