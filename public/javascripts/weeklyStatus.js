d33ae2e4dd9420d0c73619e15a5b7cacfd0166ef
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-09-13 22:47:42 -0400

    modified the edit requests

--- public/javascripts/weeklyStatus.js
+++ public/javascripts/weeklyStatus.js
@@ -3,52 +3,62 @@ document.addEventListener('DOMContentLoaded', () => {
 
   
   $(document).on("click", "#edit-status", function(event){
+    let targetStatusCard = event.currentTarget.parentNode.parentNode.parentNode.querySelectorAll('p')
+    let targetStatusCardArr = Array.from(targetStatusCard);
+
+    console.log(event.currentTarget.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.querySelectorAll('input'))
     event.preventDefault()
     let statusId = event.currentTarget.getAttribute('data-id');
     axios.get(`/api/weeklystatus/status/${statusId}`)
     .then(status => {
-      $('#edit-weekly-status input[name="tester"]').val(status.data.status.tester);
+      $('#edit-weekly-status input[name="tester"]').val(status.data.status.tester._id);
       $('#edit-weekly-status input[name="beginDate"]').val(status.data.beginDateVal);
       $('#edit-weekly-status input[name="endDate"]').val(status.data.endDateVal);
-      $('#edit-weekly-status input[name="stories"]').val(status.data.status.stories);
-      $('#edit-weekly-status input[name="bugsCreated"]').val(status.data.status.bugsCreated);
-      $('#edit-weekly-status input[name="ticketsRejected"]').val(status.data.status.ticketsRejected);
-      $('#edit-weekly-status input[name="blockers"]').val(status.data.status.blockers);
-      $('#edit-weekly-status input[name="general"]').val(status.data.status.general);
-      $('#edit-weekly-status input[name="nextWeek"]').val(status.data.status.nextWeek);
-      $('#edit-weekly-status input[name="testCases"]').val(status.data.status.testCases);
+      $('#edit-weekly-status textarea[name="stories"]').val(status.data.status.stories);
+      $('#edit-weekly-status textarea[name="bugsCreated"]').val(status.data.status.bugsCreated);
+      $('#edit-weekly-status textarea[name="ticketsRejected"]').val(status.data.status.ticketsRejected);
+      $('#edit-weekly-status textarea[name="blockers"]').val(status.data.status.blockers);
+      $('#edit-weekly-status textarea[name="general"]').val(status.data.status.general);
+      $('#edit-weekly-status textarea[name="nextWeek"]').val(status.data.status.nextWeek);
+      $('#edit-weekly-status textarea[name="testCases"]').val(status.data.status.testCases);
       $('#edit-weekly-status input[name="id"]').val(status.data.status._id);
 
     })
     .catch(err => console.log(err))
     
     $('#editStatusModal').on('shown.bs.modal', function () {
-    
       $('#myInput').trigger('focus')
     })
+
+    $(document).on("click", "#save-edits", function(event){
+      console.log(targetStatusCard)
+      event.preventDefault()
+      let editWeeklyStatus = $('#edit-weekly-status').serializeArray()
+      axios.put(`/api/weeklystatus/update/${editWeeklyStatus[10].value}`, editWeeklyStatus)
+     .then(response => {
+        axios.get(`/api/weeklystatus/status/${response.data[1]._id}`)
+        .then(status => {
+          let data = status.data
+          // ${data.formattedUpdatedDate}
+          // ${data.status.tester.firstName} ${data.status.tester.lastName}
+          // ${data.formattedBeginDate} - ${status.data.formattedEndDate}
+          console.log(data)
+          targetStatusCard[0].innerHTML = `${data.status.stories}`
+          targetStatusCard[1].innerHTML = `${data.status.bugsCreated}`
+          targetStatusCard[2].innerHTML = `${data.status.ticketsRejected}`
+          targetStatusCard[3].innerHTML =  `${data.status.blockers}`
+          targetStatusCard[4].innerHTML = `${data.status.general}`
+          targetStatusCard[5].innerHTML = `${data.status.nextWeek}`
+          targetStatusCard[6].innerHTML = `${data.status.testCases}`
+        })
+        .catch(err => console.log(err))
+         })
+     .catch(err => console.log(err))
+  
+     });
  });
 
- $(document).on("click", "#save-edits", function(event){
-    event.preventDefault()
-    let statusId = $(event.currentTarget.parentNode.parentNode).find('input:hidden')[1].value
-    let weeklyStatus = $(event.currentTarget.parentNode.parentNode).find('input')
-    event.preventDefault()
-    axios.put(`/api/weeklystatus/update/${statusId}`, {
-     tester: weeklyStatus.first().val(),
-     beginDate: weeklyStatus.eq(1).val() ,
-     endDate: weeklyStatus.eq(2).val() ,
-     bugsCreated: weeklyStatus.eq(4).val() ,
-     ticketsRejected: weeklyStatus.eq(5).val() ,
-     stories: weeklyStatus.eq(3).val() ,
-     blockers: weeklyStatus.eq(6).val() ,
-     general: weeklyStatus.eq(7).val() ,
-     nextWeek: weeklyStatus.eq(8).val() ,
-     testCases: weeklyStatus.eq(9).val() 
-   })
-   .then(response => {})
-   .catch(err => console.log(err))
-
-   });
+ 
   
   $(document).on("click", "#delete-status", function(event){
     let statusId = event.currentTarget.getAttribute('data-id');
