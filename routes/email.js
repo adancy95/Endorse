41cdab7d239b5f8de6e2798e2b77946d5d569f0e
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-09-16 08:40:15 -0400

    updated email message

--- routes/email.js
+++ routes/email.js
@@ -2,24 +2,21 @@ const nodemailer = require('nodemailer');
 
 let transporter = nodemailer.createTransport({
   service: 'Gmail',
+  host: 'smtp.gmail.com',
+  secure: false,
   auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD 
+    user:  process.env.EMAIL_USERNAME,
+    pass: process.env.EMAIL_PASSWORD
   }
 });
 
-let mailOptions = {
+transporter.sendMail({
   from: process.env.EMAIL_USERNAME,
-  to: process.env.Email_RECIPIENT,
-  subject: "testing emails",
-  text: "It worked"
-
-}
-
-transporter.sendMail(mailOptions, function(err, data){
-  if(err){
-    console.log("error ocurred", err)
-  }else{
-    console.log("email sent")
-  }
+  to:  process.env.Email_RECIPIENT, 
+  subject: 'Awesome Subject', 
+  text: 'Awesome Message',
+  html: '<b>Awesome Message</b>'
 })
+.then(info => console.log(info))
+.catch(error => console.log(error))
+
