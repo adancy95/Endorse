b6558cd2a3bade015d55dfe29c386fa8ebdba6ef
adancy95 <antdancy95@gmail.com>
2021-04-11 14:07:45 -0400

    changed database connection to use mongoclient instead of mongoose

--- app.js
+++ app.js
@@ -1,74 +1,79 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const bodyParser   = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
-const session      = require("express-session");
-const bcrypt       = require("bcryptjs");
-const User         = require("./models/user");
-const passport     = require("passport");
-const LocalStrategy= require("passport-local").Strategy;
-const flash        = require("connect-flash");
-const methodOverride = require('method-override')
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const favicon = require("serve-favicon");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+const MongoClient = require("mongodb").MongoClient;
+const logger = require("morgan");
+const path = require("path");
+const session = require("express-session");
+const bcrypt = require("bcryptjs");
+const User = require("./models/user");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const flash = require("connect-flash");
+const methodOverride = require("method-override");
 const Handlebars = require("handlebars");
-const moment = require('moment');
+const moment = require("moment");
 const MomentHandler = require("handlebars.moment");
 MomentHandler.registerHelpers(Handlebars);
 
-
-
 mongoose
-  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
   })
   .catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error("Error connecting to mongo", err);
   });
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const app_name = require("./package.json").name;
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
+);
 
 const app = express();
 
 // Middleware Setup
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(methodOverride('_method'))
+app.use(methodOverride("_method"));
 
 // Express View engine setup
 
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
+app.use(
+  require("node-sass-middleware")({
+    src: path.join(__dirname, "public"),
+    dest: path.join(__dirname, "public"),
+    sourceMap: true
+  })
+);
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
-
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
 // default value for title local
-app.locals.title = '#daWeekly -- If it isn’t written down, it didn’t happen.';
+app.locals.title = "#daWeekly -- If it isn’t written down, it didn’t happen.";
 
-
-
-app.use(session({
-  secret: "endorse",
-  resave: true,
-  saveUninitialized: true
-}));
+app.use(
+  session({
+    secret: "endorse",
+    resave: true,
+    saveUninitialized: true
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -79,45 +84,45 @@ passport.serializeUser((user, cb) => {
 
 passport.deserializeUser((id, cb) => {
   User.findById(id, (err, user) => {
-    if (err) { return cb(err); }
+    if (err) {
+      return cb(err);
+    }
     cb(null, user);
   });
 });
 
 app.use(flash());
 
-passport.use(new LocalStrategy((username, password, next) => {
-  User.findOne({ username }, (err, user) => {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      return next(null, false, { message: "Incorrect username" });
-    }
-    if (!bcrypt.compareSync(password, user.password)) {
-      return next(null, false, { message: "Incorrect password" });
-    }
+passport.use(
+  new LocalStrategy((username, password, next) => {
+    User.findOne({ username }, (err, user) => {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        return next(null, false, { message: "Incorrect username" });
+      }
+      if (!bcrypt.compareSync(password, user.password)) {
+        return next(null, false, { message: "Incorrect password" });
+      }
 
-    return next(null, user);
-  });
-}));
-
-
+      return next(null, user);
+    });
+  })
+);
 
 app.use((req, res, next) => {
   res.locals.currentUser = req.user;
-  res.locals.errorMessage = req.flash('error');
-  res.locals.successMessage=req.flash('success');
-  res.locals.moment = require('moment');
+  res.locals.errorMessage = req.flash("error");
+  res.locals.successMessage = req.flash("success");
+  res.locals.moment = require("moment");
   next();
 });
 
-
-app.use('/', require('./routes/index'));
-app.use('/', require('./routes/users'));
-app.use('/', require('./routes/testArtifacts'));
-app.use('/', require('./routes/projects'));
-app.use('/', require('./routes/weeklyStatus'));
-
+app.use("/", require("./routes/index"));
+app.use("/", require("./routes/users"));
+app.use("/", require("./routes/testArtifacts"));
+app.use("/", require("./routes/projects"));
+app.use("/", require("./routes/weeklyStatus"));
 
 module.exports = app;
